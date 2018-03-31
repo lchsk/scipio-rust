fb475832caca9ef203e1a86b47c08844cdf0e4c5
lchsk <mjlechowski@gmail.com>
2018-04-01 00:27:16 +0100

    Create files as part of "create" command

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,47 @@ extern crate markdown;
 extern crate clap;
 use clap::{Arg, App, SubCommand};
 use std::fs;
+use std::path::Path;
+use std::fs::{OpenOptions};
 
 fn create_dir(dir_name: &str) {
     match fs::create_dir(dir_name) {
-        Err(why) => println!("Unable to create '{}': {:?}", dir_name, why.kind()),
+        Err(e) => println!("Unable to create '{}': {:?}", dir_name, e.kind()),
         Ok(_) => {},
     }
 
+}
+
+
+fn touch(filename: &str) {
+    let path = Path::new(filename);
+
+    match OpenOptions::new().create(true).write(true).open(path) {
+        Ok(_) => {},
+        Err(e) => println!("Unable to create file '{}': {:?}", filename, e),
+    }
+}
+
+fn create_new_project(project_name: &str) {
+    println!("Creating new project '{}'", project_name);
+
+    create_dir(project_name);
+
+    let source_dir = format!("{}/{}", project_name, "source");
+
+    create_dir(&source_dir);
+
+    let posts_dir = format!("{}/{}", project_name, "source/posts");
+    let pages_dir = format!("{}/{}", project_name, "source/pages");
+
+    create_dir(&posts_dir);
+    create_dir(&pages_dir);
+
+    let config_file_dir = format!("{}/{}", project_name, "scipio.config");
+    let index_dir = format!("{}/{}", project_name, "source/index.md");
+
+    touch(&config_file_dir);
+    touch(&index_dir);
 }
 
 fn main() {
@@ -32,15 +66,6 @@ fn main() {
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("create") {
-        let project_name = matches.value_of("project_name").unwrap_or("scipio_default");
-        println!("Creating new project '{}'", project_name);
-
-        create_dir(project_name);
-
-        let posts_dir = format!("{}/{}", project_name, "posts");
-        let pages_dir = format!("{}/{}", project_name, "pages");
-
-        create_dir(&posts_dir);
-        create_dir(&pages_dir);
+        create_new_project(matches.value_of("project_name").unwrap_or("scipio_default"));
     }
 }
