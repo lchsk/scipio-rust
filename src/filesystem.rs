aaea1934a83b67aea1d8c51f10ead5d271083e29
lchsk <mjlechowski@gmail.com>
2018-06-02 02:20:29 +0100

    Add tests for filesystem

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -33,5 +33,74 @@ pub fn clean_build(project_name: &str) {
         .arg("-r")
         .arg(format!("{}/build", project_name))
         .output()
-        .expect("failed to execute process");
+        .expect("failed to remove build directory");
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static TEST_DIR: &str = "test_dir";
+
+    fn setup() {
+        create_dir(TEST_DIR);
+    }
+
+    fn teardown() {
+        Command::new("rm")
+            .arg("-r")
+            .arg(TEST_DIR)
+            .output()
+            .expect("failed to remove");
+    }
+
+    #[test]
+    fn create_file() {
+        setup();
+
+        let path = &format!("{}/test_fle", TEST_DIR);
+        touch(path);
+
+        let meta = fs::metadata(path).unwrap();
+        assert!(meta.is_file());
+
+        teardown()
+    }
+
+    #[test]
+    fn create_directory() {
+        setup();
+
+        let meta = fs::metadata(TEST_DIR).unwrap();
+        assert!(meta.is_dir());
+
+        teardown()
+    }
+
+    #[test]
+    fn delete_build() {
+        setup();
+
+        let path = &format!("{}/build", TEST_DIR);
+        create_dir(path);
+
+        let meta = fs::metadata(path).unwrap();
+        assert!(meta.is_dir());
+
+        clean_build(TEST_DIR);
+
+        let meta_result = fs::metadata(path);
+
+        match meta_result {
+            Ok(_) => {
+                assert!(false);
+            }
+            Err(_) => {
+                assert!(true);
+            }
+        }
+
+        teardown()
+    }
 }
